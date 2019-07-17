c96374f516c51a862d65c5e846663dd287a1b0fe
Scoutzknifez <Cody@4alexanders.com>
2019-07-17 14:22:01 -0700

    Added a comment to fetched data.

--- src/DataFetcher/FetchedData.java
+++ src/DataFetcher/FetchedData.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class FetchedData {
     // Last fetched and parsed data
-    public static CurrentWeather currentWeather = null;
-    public static ArrayList<HourWeather> hourWeathers = null; // Appears to be always length 169 aka index 0 - 168 inclusive
+    public static CurrentWeather currentWeather = null; // The current weather at the location
+    public static ArrayList<HourWeather> hourWeathers = null; // Appears to be always length 169 aka index 0 - 168 inclusive | 0 is the hour you are in stopping at the top of hour, 1 is the coming up hour
     public static ArrayList<DayWeather> dayWeathers = null; // Index 0 is today, index 7 is one week from today
 }
