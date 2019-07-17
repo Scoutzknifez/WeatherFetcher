f499e591d18ba5a51aa9c24d3885c03d5078dda8
Scoutzknifez <cody@4alexanders.com>
2019-07-17 15:56:12 -0700

    Imported the has internet check from previous apps.

--- src/Utility/Constants.java
+++ src/Utility/Constants.java
@@ -1,6 +1,9 @@
 package Utility;
 
 public class Constants {
+    // Program wide variables
+    public static boolean hasInternet = false;
+
     // Weather Fetching Constants
     public static final String WEB_SERVER = "https://api.darksky.net/forecast/";
     public static final String API_KEY = "948fb757148e84bcba59004551b903ec";
--- src/Utility/Utils.java
+++ src/Utility/Utils.java
@@ -2,16 +2,41 @@ package Utility;
 
 import DataFetcher.FetcherController;
 
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class Utils {
     public static long getMillisFromEpoch(long epoch) {
         return epoch * Constants.MillisInSecond;
     }
 
     public static void initializeApplication() {
-        FetcherController.fetchWeather();
+        Constants.hasInternet = hasInternetConnection();
+        if(Constants.hasInternet) {
+            FetcherController.fetchWeather();
+        } else {
+            System.out.println("No internet connection!");
+        }
     }
 
     public static boolean isEmptyJsonString(String string) {
         return (string == null || string.equalsIgnoreCase("") || string.equalsIgnoreCase("{}"));
+    }
+
+    public static boolean hasInternetConnection() {
+        boolean status = false;
+        Socket socket = new Socket();
+        InetSocketAddress address = new InetSocketAddress("www.google.com", 80);
+        try {
+            socket.connect(address, 1500);
+            if(socket.isConnected())
+                status = true;
+        } catch (Exception e) {
+        } finally {
+            try {
+                socket.close();
+            } catch (Exception e) {}
+        }
+        return status;
     }
 }
