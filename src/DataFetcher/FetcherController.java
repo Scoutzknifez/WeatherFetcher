6c1830efad3226dd9e0633fd937ab47049a7c56f
Scoutzknifez <codygaming@4alexanders.com>
2020-01-02 00:24:00 -0800

    Removed unused import

--- src/DataFetcher/FetcherController.java
+++ src/DataFetcher/FetcherController.java
@@ -11,7 +11,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 
 public class FetcherController {
